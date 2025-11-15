28698dfd9987ef7a9f43cb5559a75cce181d0ba8
hal3i <104970044+hal3i@users.noreply.github.com>
2025-11-15 21:18:46 +0900

    減算、乗算、除算を追加

--- main.c
+++ main.c
@@ -13,9 +13,27 @@ void test_add_operator(void)
     TEST_ASSERT_EQUAL(7, 5 + 2);
 }
 
+void test_sub_operator(void)
+{
+    TEST_ASSERT_EQUAL(3, 5 - 2);
+}
+
+void test_mul_operator()
+{
+    TEST_ASSERT_EQUAL(6, 2 * 3);
+}
+
+void test_div_operator()
+{
+    TEST_ASSERT_EQUAL(4, 8 / 2);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
     RUN_TEST(test_add_operator);
+    RUN_TEST(test_sub_operator);
+    RUN_TEST(test_mul_operator);
+    RUN_TEST(test_div_operator);
     return UNITY_END();
 }
