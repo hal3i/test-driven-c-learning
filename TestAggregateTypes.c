c2b64c520a85f6a591ff76a0489cd30646c277da
hal3i <104970044+hal3i@users.noreply.github.com>
2025-12-05 20:34:30 +0900

    2次元配列の初期化を追加

--- TestAggregateTypes.c
+++ TestAggregateTypes.c
@@ -17,3 +17,23 @@ void test_array_initialization(void)
     TEST_ASSERT_EQUAL(1, y[1]);
     TEST_ASSERT_EQUAL(2, y[2]);
 }
+
+void test_2d_array_initialization(void)
+{
+    int A[2][3];
+    A[0][0] = 0;
+    A[0][1] = 1;
+    A[0][2] = 2;
+    A[1][0] = 10;
+    A[1][1] = 11;
+    A[1][2] = 12;
+
+    int B[2][3] = { 0, 1, 2, 10, 11, 12 };
+
+    TEST_ASSERT_EQUAL(A[0][0], B[0][0]);
+    TEST_ASSERT_EQUAL(A[0][1], B[0][1]);
+    TEST_ASSERT_EQUAL(A[0][2], B[0][2]);
+    TEST_ASSERT_EQUAL(A[1][0], B[1][0]);
+    TEST_ASSERT_EQUAL(A[1][1], B[1][1]);
+    TEST_ASSERT_EQUAL(A[1][2], B[1][2]);
+}
--- test_runners/TestAggregateTypes_Runner.c
+++ test_runners/TestAggregateTypes_Runner.c
@@ -7,6 +7,7 @@
 extern void setUp(void);
 extern void tearDown(void);
 extern void test_array_initialization(void);
+extern void test_2d_array_initialization(void);
 
 
 /*=======Mock Management=====*/
@@ -75,6 +76,7 @@ int main(void)
 {
   UnityBegin("TestAggregateTypes.c");
   run_test(test_array_initialization, "test_array_initialization", 3);
+  run_test(test_2d_array_initialization, "test_2d_array_initialization", 21);
 
   return UNITY_END();
 }
